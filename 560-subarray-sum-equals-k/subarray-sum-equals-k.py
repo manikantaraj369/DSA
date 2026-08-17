b9971f759e8c3ee0c1f62dc614cbed5ccfb1e0be
Manikanta Raju <nmanikantaraju369@gmail.com>
2026-08-17 12:13:29 +0530

    Time: 29 ms (82.48%) | Memory: 14.7 MB (46.83%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,13 +1,10 @@
 class Solution(object):
     def subarraySum(self, nums, k):
-        n = len(nums)
         count,summ = 0 , 0
-        hashmap = {}
-        hashmap[0] = 1
-        for i in range(n):
-            summ += nums[i]
-            rem = summ - k
-            if rem in hashmap :
-                count += hashmap[rem]
+        hashmap = {0:1}
+        for i in nums:
+            summ += i
+            if (summ - k) in hashmap :
+                count += hashmap[summ - k]
             hashmap[summ] = hashmap.get(summ,0) + 1
         return count 
