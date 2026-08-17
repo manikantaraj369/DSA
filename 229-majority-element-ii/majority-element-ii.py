b77de7073d7535ea6328f16de62c373383965613
Manikanta Raju <nmanikantaraju369@gmail.com>
2026-08-17 18:24:50 +0530

    Time: 10 ms (64.38%) | Memory: 14 MB (91.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,31 +1,23 @@
 class Solution(object):
     def majorityElement(self, nums):
-        elm1,elm2 = 0,0
+        elm1,elm2 = None,None
         count1 ,count2 = 0 , 0
         n = len(nums)
-        for i in range(n):
-            if count1 == 0 and nums[i] != elm2:
-                count1 = 1
-                elm1 = nums[i]
-            elif count2 == 0 and nums[i] != elm1:
-                count2 = 1
-                elm2 = nums[i]
-            elif elm1 == nums[i]:
+        for i in nums:
+            if count1 == 0 and i != elm2:
+                elm1 ,count1= i,1
+            elif count2 == 0 and i != elm1:
+                elm2 ,count2= i,1
+            elif elm1 == i:
                 count1 += 1
-            elif elm2 == nums[i]:
+            elif elm2 == i:
                 count2 += 1
             else :
                 count1 -= 1
                 count2 -= 1
-        count1 , count2 = 0,0
-        for i in range(n):
-            if nums[i] == elm1:
-                count1 += 1
-            elif nums[i] == elm2:
-                count2 += 1
         lis = []
-        if count1 > (n//3):
+        if nums.count(elm1) > (n//3):
             lis.append(elm1)
-        if count2 > (n//3):
+        if nums.count(elm2) > (n//3):
             lis.append(elm2)
         return lis
