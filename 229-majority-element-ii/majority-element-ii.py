9885c04fd8ba155300563fd15b7a7b472206062a
Manikanta Raju <nmanikantaraju369@gmail.com>
2026-08-18 10:30:10 +0530

    Time: 13 ms (40.46%) | Memory: 14 MB (82.74%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -2,7 +2,6 @@ class Solution(object):
     def majorityElement(self, nums):
         elm1,elm2 = None,None
         count1 ,count2 = 0 , 0
-        n = len(nums)
         for i in nums:
             if count1 == 0 and i != elm2:
                 elm1 ,count1= i,1
@@ -16,8 +15,8 @@ class Solution(object):
                 count1 -= 1
                 count2 -= 1
         lis = []
-        if nums.count(elm1) > (n//3):
+        if nums.count(elm1) > (len(nums)//3):
             lis.append(elm1)
-        if nums.count(elm2) > (n//3):
+        if nums.count(elm2) > (len(nums)//3):
             lis.append(elm2)
         return lis
